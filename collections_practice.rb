7ca336e054af7b1406f458c1eceb4da2fa0a67c0
Phoebe Lau <phoebelau7@gmail.com>
2017-11-29 22:46:13 +0000

    automatically backed up by learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -28,7 +28,7 @@ end
 
 def kesha_maker(array)
   new_array = []
-  array.each {|name| new_array << name.sub!(/(?<=.{2})./, "$")
+  array.each {|name| new_array << name[2]="$"
   }
   new_array
 end
