27f1977d75cc60a509759cf2ab61e44f3c76e760
Phoebe Lau <phoebelau7@gmail.com>
2017-11-29 23:00:51 +0000

    automatically backed up by learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -18,7 +18,7 @@ def swap_elements(array)
 end
 
 def swap_elements_from_to(array, index, destination_index)
-  array.sort {|a,b| a[0] <=> b[2]}
+  array[index] = array[destination_index]
 end
 
 def reverse_array(array)
