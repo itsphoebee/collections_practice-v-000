44bc95dee02b43b30c8a06358745e7a3fe2ca649
Phoebe Lau <phoebelau7@gmail.com>
2017-11-29 23:07:11 +0000

    automatically backed up by learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -18,7 +18,8 @@ def swap_elements(array)
 end
 
 def swap_elements_from_to(array, index, destination_index)
-  array[index] <=> array[destination_index]
+  new_array = []
+  new_array << array[index] <=> array[destination_index]
   array
 end
 
